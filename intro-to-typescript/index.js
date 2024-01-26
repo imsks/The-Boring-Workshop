1a4d8f3d402878faede0fc0d8ecd277c65e5710e
Sachin <sachinkshuklaoo7@gmail.com>
2024-01-26 12:30:09 +0530

    Typescript Setup Working

---
+++ intro-to-typescript/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function addToNumbers(a, b) {
+    return a + b;
+}
+const varA = 2;
+const varB = 5;
+const result = addToNumbers(varA, varB);
+function storeIntoDB(value) {
+    // Store Logic
+}
+storeIntoDB(result);
---
+++ intro-to-typescript/index.ts
@@ -0,0 +1,14 @@
+function addToNumbers(a: number, b: number) {
+    return a + b
+}
+
+const varA = 2
+const varB = 5
+
+const result = addToNumbers(varA, varB)
+
+function storeIntoDB(value: any) {
+    // Store Logic
+}
+
+storeIntoDB(result)
